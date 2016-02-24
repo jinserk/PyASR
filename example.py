9514e837fb83ad9bd8ca896478b035e9ee033ca7
Jinserk Baik <jinserk.baik@gmail.com>
2016-02-25 08:11:04 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,14 @@
-from features import mfcc
-from features import logfbank
+import os.path as op
+from pyasr.features import mfcc
+from pyasr.features import logfbank
 import scipy.io.wavfile as wav
 
-(rate,sig) = wav.read("file.wav")
+timit_base = "/home/leo/work/neural/timit/TIMIT"
+wav_file = op.join(timit_base, "TRAIN/DR1/FCJF0/SI1027.WAV")
+
+print wav_file
+
+(rate,sig) = wav.read(wav_file)
 mfcc_feat = mfcc(sig,rate)
 fbank_feat = logfbank(sig,rate)
 
