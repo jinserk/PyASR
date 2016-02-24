563f5f876f02e2cd42a03063eb644cb115f0d106
Jinserk Baik <jinserk.baik@gmail.com>
2016-02-25 06:06:43 +0900

    import functions from credits

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyasr',
+      version='0.1',
+      description='Python Automatic Speech Recognition',
+      author='Jinserk Baik',
+      url='https://github.com/jinserk/PyASR',
+      packages=['pyasr'],
+    )
